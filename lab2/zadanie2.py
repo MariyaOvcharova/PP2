3de991379b3f8a177521fffdc7dc7aca28fc2917
Mariya <m_ovcharova@kbtu.kz>
2024-01-26 10:34:25 +0600

    Lab2

--- lab2/zadanie2.py
+++ lab2/zadanie2.py
@@ -1,1 +1,25 @@
 x=200
+y=155
+
+print(x+y)
+print(x%y)
+print(x/y) 
+"""не округляет"""
+print(x//y) 
+"""округляет"""
+
+c=6
+b=7
+if(c!=b and c<b):
+    print("Yes")
+else:
+    print("No")
+
+
+if x is y :
+    print("Yes")
+else:
+    print("No")
+
+
+print(x|y)
