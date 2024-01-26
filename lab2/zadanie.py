b8ed69188a1cbfce0b0e598928c8cb4edaf91e2e
Mariya <m_ovcharova@kbtu.kz>
2024-01-26 09:35:49 +0600

    Lab2

--- lab2/zadanie.py
+++ lab2/zadanie.py
@@ -9,3 +9,37 @@ if b<a:
 else:
     print(b, "bigger")
 
+print(bool("blobs"))
+print(bool(123))
+print(bool(["yabloko", "vishnya", "banan"]))
+
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+
+
+def myclass():
+    return 0
+
+m=myclass()
+print(m)
+
+def myclass():
+    return True
+
+m=myclass()
+print(m)
+
+if myclass():
+    print("DA")
+else:
+    print("No(")
+
+
+
+
